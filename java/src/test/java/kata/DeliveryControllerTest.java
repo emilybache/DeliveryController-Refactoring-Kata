cd1d1e95b76dd77cd3653dfbcd4d4cd060947ba4
Lars Eckart <lars.eckart@hey.com>
2022-02-09 12:11:06 +0200

    remove gradle config for  preview features

--- java/src/test/java/kata/DeliveryControllerTest.java
+++ java/src/test/java/kata/DeliveryControllerTest.java
@@ -1,10 +1,13 @@
 package kata;
 
+import com.github.larseckart.tcr.TestCommitRevertMainExtension;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+//@ExtendWith(TestCommitRevertMainExtension.class)
 class DeliveryControllerTest {
 
   private String result;
